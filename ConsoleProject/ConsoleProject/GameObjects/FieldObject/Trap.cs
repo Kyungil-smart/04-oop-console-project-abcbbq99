d151a14ff98beb967d86dd9ccc4a67a3fd3a2145
abcbbq99 <kim3373613@gmail.com>
2026-01-08 17:57:51 +0900

    [perf] 포션과 트랩 입력값 만큼 회복하는것으로 변경

--- ConsoleProject/ConsoleProject/GameObjects/FieldObject/Trap.cs
+++ ConsoleProject/ConsoleProject/GameObjects/FieldObject/Trap.cs
@@ -3,16 +3,18 @@
 public class Trap : GameObject, IInteractable
 {
     public PlayerCharacter Owner { get; set; }
+    private int TrapDamage;
     
-    public Trap() => Init();
+    public Trap(int damage) => Init(damage);
     
-    private void Init()
+    private void Init(int damage)
     {
         Symbol = '▩';
+        TrapDamage = damage;
     }
     
     public void Interact(PlayerCharacter player)
     {
-        
+        Owner.TakeDamage(TrapDamage);
     }
 }
--- ConsoleProject/ConsoleProject/GameObjects/Items/Potion.cs
+++ ConsoleProject/ConsoleProject/GameObjects/Items/Potion.cs
@@ -2,17 +2,21 @@
 
 public class Potion : Item, IInteractable
 {
-    public Potion() => Init();
+    private int HealingPoint;
+    public Potion(int value) => Init(value);
     
-    private void Init()
+    private void Init(int value)
     {
         Symbol = 'I';
         Name = "Potion";
+        HealingPoint = value;
+        Description = $"{HealingPoint} 만큼 치유";
+        
     }
 
     public override void Use()
     {
-        Owner.Heal(10);
+        Owner.Heal(HealingPoint);
         
         Inventory.Remove(this);
         Inventory = null;
